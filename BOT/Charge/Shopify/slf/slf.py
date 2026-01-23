220bc7c745401ec90ec4a8b252d1a44153579a7d
Dipendr98 <83461828+Dipendr98@users.noreply.github.com>
2026-01-23 22:25:39 +0530

    Update slf.py

--- BOT/Charge/Shopify/slf/slf.py
+++ BOT/Charge/Shopify/slf/slf.py
@@ -7,56 +7,6 @@ def get_site(user_id):
     with open("DATA/sites.json", "r") as f:
         sites = json.load(f)
     return sites.get(str(user_id), {}).get("site")
-
-# async def check_card(user_id, cc, site=None):
-#     if not site:
-#         site = get_site(user_id)
-#     if not site:
-#         return "Site Not Found"
-
-#     proxy = get_proxy(user_id)
-    
-#     if proxy:
-#         url = f"http://69.62.117.8:8000/check?card={cc}&site={site}&proxy={proxy}"
-#     else:
-#         url = f"http://69.62.117.8:8000/check?card={cc}&site={site}"
-
-#     try:
-#         async with httpx.AsyncClient(timeout=100.0) as client:
-#             response = await client.get(url)
-#             data = response.json()
-
-#             print(data)
-
-#     except httpx.ReadTimeout:
-#         return "Request Timeout"
-#     except Exception:
-#         return "Request Failed"
-
-#     response_text = data.get("Response", "").upper()
-
-#     if "Server disconnected without sending a response." in response_text
-
-
-#     price = data.get("Price", "")
-#     cc_field = data.get("cc")
-
-#     # if cc_field:
-#     if price and f"ORDER_PLACED".upper() in response_text:
-#         return "ORDER_PLACED"
-#     elif "3DS_REQUIRED" in response_text:
-#         return "3DS_REQUIRED"
-#     elif "CARD_DECLINED" in response_text:
-#         return "CARD_DECLINED"
-#     elif "HEADER VALUE MUST BE STR OR BYTES, NOT" in response_text:
-#         return "Product ID ⚠️"
-#     elif "EXPECTING VALUE: LINE 1 COLUMN 1 (CHAR 0)" in response_text:
-#         return "IP Rate Limit"
-#     elif "Declined" in response_text:
-#         return "Site | Card Error "
-#     else:
-#         return response_text
-
 async def check_card(user_id, cc, site=None):
     if not site:
         site = get_site(user_id)
